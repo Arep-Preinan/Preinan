17bc547961bc39fe2d93709326af235e170a281d
shalexandeer <shalexandeer@student.telkomuniversity.ac.id>
2023-03-16 20:09:31 +0700

    gambar destinasi local

--- resources/js/Pages/Destinasi.jsx
+++ resources/js/Pages/Destinasi.jsx
@@ -137,19 +137,24 @@ const Destinasi = (props) => {
                             </Heading>
                         </div>
                         <div className="grid md:grid-cols-[auto_auto] lg:grid-cols-[auto_420px] md:gap-5 h-[540px] max-md:rounded-3xl max-md:overflow-hidden  ">
-                            
-                            <a href="#kumpulan-destinasi" className="flex hover:cursor-pointer transition  justify-center items-end md:rounded-[24px] bg-[url(http://preinan.xxuz.com/images/telaga2.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-cover">
+                            <a
+                                href="#kumpulan-destinasi"
+                                className="flex hover:cursor-pointer transition  justify-center items-end md:rounded-[24px] bg-[url(../images/telaga2.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-cover"
+                            >
                                 <h1
                                     className="md:text-4xl text-white w-full h-full grid place-items-center lg:opacity-0 lg:hover:opacity-50 bg-black bg-opacity-50 lg:hover:bg-gray-900 md:rounded-3xl transition duration-400"
                                     onClick={() => {
                                         handleActive("danau");
                                     }}
                                 >
-                                        Danau
+                                    Danau
                                 </h1>
                             </a>
                             <div className="grid grid-cols-2 md:grid-cols-1 md:gap-5">
-                                <a href="#kumpulan-destinasi" className="hover:cursor-pointer bg-[url(http://preinan.xxuz.com/images/gunung2.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-center md:rounded-[24px]">
+                                <a
+                                    href="#kumpulan-destinasi"
+                                    className="hover:cursor-pointer bg-[url(../images/gunung2.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-center md:rounded-[24px]"
+                                >
                                     <h1
                                         className="md:text-4xl text-white w-full h-full grid place-items-center lg:opacity-0 lg:hover:opacity-50 bg-black bg-opacity-50 lg:hover:bg-gray-900 md:rounded-3xl transition duration-400"
                                         onClick={() => {
@@ -159,7 +164,10 @@ const Destinasi = (props) => {
                                         Gunung
                                     </h1>
                                 </a>
-                                <a href="#kumpulan-destinasi" className="bg-[url(http://preinan.xxuz.com/images/curug.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-center md:rounded-[24px]">
+                                <a
+                                    href="#kumpulan-destinasi"
+                                    className="bg-[url(../images/curug.jpg)] bg-no-repeat bg-cover md:bg-top lg:bg-center md:rounded-[24px]"
+                                >
                                     <h1
                                         className=" hover:cursor-pointer md:text-4xl text-white w-full h-full grid place-items-center lg:opacity-0 lg:hover:opacity-50 bg-black bg-opacity-50 lg:hover:bg-gray-900 md:rounded-3xl transition duration-400"
                                         onClick={() => {
